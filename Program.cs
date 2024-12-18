70f1f8041249fc516e2bba57789fe1f0cdbd910d
falau <falau@LENOVO>
2024-12-18 18:55:43 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿
+namespace Set3Ex4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("vector: ");
+            string input = Console.ReadLine();
+            int[] v = Array.ConvertAll(input.Split(' '),int.Parse);
+
+            int Min = int.MaxValue;
+            int Max = int.MinValue;
+            int minCount = 0;
+            int maxCount = 0;
+            int n = v.Length;
+      
+            foreach(int num in v)
+            {
+                if(num < Min)
+                {
+                    Min = num;
+                    minCount = 1;
+                }
+                else if(num == Min)
+                {
+                    minCount++;
+                }
+                if(num > Max)
+                {
+                    Max = num;
+                    maxCount = 1;
+                }
+                else if(num == Max)
+                {
+                    maxCount++;
+                }
+
+            }
+
+            Console.WriteLine($"Minim: {Min} ; Maxim: {Max}; Aparitii Minim: {minCount} ; Aparitii Maxim: {maxCount}");
+        }
+    }
+}
